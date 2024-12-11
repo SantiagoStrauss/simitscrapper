56be40aa0582fedada4eb9457a1a91b8339e3341
SantiagoStrauss <ds.ramos@uniandes.edu.co>
2024-12-11 18:36:07 -0500

    s4

--- render-build.sh
+++ render-build.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
-# Instalar Chrome
+# Instalación de Chrome
 echo "Instalando Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get update
-sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+apt-get update
+apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Instalar ChromeDriver
+# Instalación de ChromeDriver
 echo "Instalando ChromeDriver"
 CHROME_DRIVER_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
-sudo mv chromedriver /usr/local/bin/
+mv chromedriver /usr/local/bin/
 
-# Limpiar
+# Limpieza
 rm google-chrome-stable_current_amd64.deb
 rm chromedriver_linux64.zip
+
+# Verificación de instalación
+echo "Versión de Chrome instalada:"
+google-chrome --version
+echo "Versión de ChromeDriver instalada:"
+chromedriver --version
