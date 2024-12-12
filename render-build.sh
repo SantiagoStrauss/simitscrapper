6b7fff2b8ede86ad348445cdce2713ee2a91ded6
SantiagoStrauss <ds.ramos@uniandes.edu.co>
2024-12-11 19:21:04 -0500

    s15

--- render-build.sh
+++ render-build.sh
@@ -2,8 +2,8 @@
 # Instalación de Chrome
 echo "Instalando Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get update
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+apt-get update || echo "apt-get update falló debido a permisos."
+apt-get install -y ./google-chrome-stable_current_amd64.deb || echo "apt-get install falló debido a permisos."
 
 # Instalación de ChromeDriver
 echo "Instalando ChromeDriver"
@@ -11,7 +11,11 @@ CHROME_DRIVER_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LAT
 wget https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
-mv chromedriver /usr/local/bin/
+mkdir -p $HOME/bin
+mv chromedriver $HOME/bin/chromedriver
+
+# Agregar ChromeDriver al PATH
+export PATH=$HOME/bin:$PATH
 
 # Limpieza
 rm google-chrome-stable_current_amd64.deb
@@ -19,6 +23,6 @@ rm chromedriver_linux64.zip
 
 # Verificación de instalación
 echo "Versión de Chrome instalada:"
-google-chrome --version
+$HOME/bin/google-chrome --version || echo "google-chrome no encontrado"
 echo "Versión de ChromeDriver instalada:"
-chromedriver --version
+chromedriver --version || echo "chromedriver no encontrado"
