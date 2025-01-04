419290c751d5f7acc773e893b6dcb13b56180d81
SantiagoStrauss <ds.ramos@uniandes.edu.co>
2025-01-04 15:17:20 -0500

    s6

--- simit.py
+++ simit.py
@@ -62,6 +62,7 @@ class simitScraper:
         options.add_argument('--window-size=1024,768')  # Reduced from 1920,1080
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
+        options.add_argument('--disable-software-rasterizer')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-extensions')
         options.add_argument('--disable-blink-features=AutomationControlled')
